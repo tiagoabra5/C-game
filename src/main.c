6ac0b9df0f2c3fcd10c349016b902105652d2048
Tiago Abraão <125285649+tiagoabra5@users.noreply.github.com>
2025-05-06 17:46:10 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,27 +1,51 @@
-#include <string.h>
-#include "screen.h"
-#include "keyboard.h"
-#include "timer.h"
+#include "game.h"
+#include <stdlib.h>
+
+void handleInput(GameState *game);
 
 int main() {
-    static int ch = 0;
-    screenInit(1);
-    keyboardInit();
-    timerInit(50);
-    screenUpdate();
+    cli_init();
+    cli_hidecursor();
+    
+    GameState game;
+    initGame(&game);
+    
+    while (1) {
+        handleInput(&game);
+        updateGame(&game);
+        renderGame(&game);
+        cli_sleep(10);
+    }
+    
+    cleanupGame(&game);
+    cli_showcursor();
+    return 0;
+}
 
-    while (ch != 10) {
-        if (keyhit()) {
-            ch = readch();
-            screenUpdate();
-        }
-    if (timerTimeOver() == 1) {
-        screenUpdate();
+void handleInput(GameState *game) {
+    if (cli_kbhit()) {
+        char key = cli_getch();
+        switch (key) {
+            case 'a':
+                movePlayer(&game->player, -1);
+                break;
+            case 'd':
+                movePlayer(&game->player, 1);
+                break;
+            case 'r':
+                if (game->player.gameOver) {
+                    cleanupGame(game);
+                    initGame(game);
+                }
+                break;
+            case 'p':
+                game->paused = !game->paused;
+                break;
+            case 'q':
+                cleanupGame(game);
+                cli_showcursor();
+                exit(0);
+                break;
         }
     }
-
-    keyboardDestroy();
-    screenDestroy();
-    timerDestroy();
-return 0;
 }
